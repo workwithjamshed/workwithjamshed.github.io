28138ce9fdbd1b442009929f15a8d60d43d963a0
workwithjamshed <workwithjamshed@gmail.com>
2026-02-28 00:19:44 +0000

    Removed Script because didn't needed it, added pure css.

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-
-// Create a script element
-const tailwindScript = document.createElement('script');
-tailwindScript.src = "https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4";
-tailwindScript.async = true;
-
-// Append it to the document head
-document.head.appendChild(tailwindScript);
